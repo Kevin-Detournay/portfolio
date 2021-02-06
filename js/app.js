2b2768da8eab4155b29f3e7e17fff684d893101b
Kevin-Detournay <kevindetournay@hotmail.fr>
2021-02-06 14:43:24 +0100

    nouveau design projet

--- js/app.js
+++ js/app.js
@@ -5,27 +5,37 @@
 
 const app = {
   scrollFunction:()=>{
-    
-    document.onscroll=()=>{
-      
-      if (document.documentElement.scrollTop >window.screen.height) {
+      document.onscroll=()=>{
+        if (document.documentElement.scrollTop >window.screen.height) {
         document.querySelector(".contact").style.top="0"
       }else{
         document.querySelector(".contact").style.top="-50px"
       }
     }
-    
-
-
   },
 
-    
+    toggleIsHidden:()=>{
+      const items=document.querySelectorAll('.item')
+      
+      items.forEach(item => {
+     
+        item.addEventListener('mouseover',()=>{
+         const appear=item.querySelector(".item__description")
+         appear.classList.remove('is-hidden')
+      
+        })
+        item.addEventListener('mouseout',()=>{
+         item.querySelector(".item__description").classList.add('is-hidden')
+         })       
+      })
+      
+    },
 
   init:()=>{
     app.scrollFunction()
     AOS.init()
     new Rellax('.rellax')
-    
+    app.toggleIsHidden()
   
   },
 
